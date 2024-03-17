ed65662bdb47e4e27f56e556db10529e8576fa0e
Rohit Malhotra <rohitvinodmalhotra@gmail.com>
2024-03-17 07:23:06 -0400

    adding prompting

--- accessibility/src/app/api/navigate/route.js
+++ accessibility/src/app/api/navigate/route.js
@@ -1,4 +1,6 @@
 import { chromium } from "playwright";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
 import fs from "fs";
 import path from "path";
 
@@ -135,4 +137,75 @@ export async function POST(request) {
       "Content-Type": "text/html",
     },
   });
+}
+
+
+
+// Inputs
+
+// isusses: list of accessibility issues in the format [{error: string, 
+                                                      //   description: string, 
+                                                      //   change: string, 
+                                                      //   incorrect elements: List[List[string]]
+                                                      // }]
+// websiteName: url for website
+// image: screenshot of page
+// fewShot: boolean to use fewShot prompting
+
+export async function fixIssues(issues, websiteName, image, fewShot) {
+
+  const genAI = new GoogleGenerativeAI(
+    "YOUR-API-KEY-HERE"
+  );
+
+  const fetchData = async () => {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const systemPrompt = `You are helpful assistant who will correct accessibility issues of a provided website.
+
+    Provide your thought before you provide a fixed version of the result.
+    
+    E.g.
+    Incorrect: [['<h3></h3>', '<h3></h3>', '<h3></h3>']]
+    Thought: because ... I will ...
+    Correct: [['<h3>Some heading</h3>', '<h3>Some heading</h3>', '<h3>Some heading</h3>']]
+    
+    
+
+    User Message:
+    You are operating on this website: ` + websiteName + "\n\n";
+
+    var issuePrompt = ""
+    for (var i = 0; i < issues.length; i ++){
+      const issue = issues[i];
+      issuePrompt += "Error: " + issue.error + "\n";
+      issuePrompt += "Description: " + issue.description + "\n";
+      issuePrompt += "Suggested change: " + issue.change + "\n";
+      issuePrompt += "Incorrect: " + issue.incorrect + "\n\n";
+
+    }
+
+
+    const finalPrompt = systemPrompt + issuePrompt;
+
+
+    const result = await model.generateContent(finalPrompt);
+    const response = await result.response;
+    const text = response.text();
+    return text;
+  };
+
+  if (image) {
+
+  }
+
+
+
+  return fetchData(issues);
+
+
+
+
+
+  
+
 }
